9ab1a3376fe4dd72b1bf270f2e581c5ff4792b45
NeverMind109 <vladgladkih123@gmail.com>
2022-05-29 03:58:15 +0300

    Created:Added Adaptive Response + Validation Check

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -2,11 +2,21 @@ import Swiper, { Navigation, Pagination } from "swiper";
 Swiper.use([Navigation, Pagination]);
 
 const projectsSlider = new Swiper(".projects__slider", {
-  slidesPerView: 3,
-  spaceBetween: 34,
+  slidesPerView: 1,
+  spaceBetween: 0,
   speed: 500,
   pagination: {
     el: ".projects__pagination",
     type: "bullets",
+  },
+  breakpoints: {
+    577: {
+      slidesPerView: 2,
+      spaceBetween: 34,
+    },
+    769: {
+      slidesPerView: 3,
+      spaceBetween: 34,
+    },
   },
 });
